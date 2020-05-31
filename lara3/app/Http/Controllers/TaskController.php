7d1be9b2378cc2901bad5e4c8a7762f8a74ad65e
Infy <infarion@gmail.com>
2020-05-31 17:19:50 +0300

    add controller

--- lara3/app/Http/Controllers/TaskController.php
+++ lara3/app/Http/Controllers/TaskController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App;
 class TaskController extends Controller
 {
     //
+
+    public function index(){
+    $tasks = App\Task::all();
+    //  $tasks=App\Task::incomplete();
+     return view('welcome',compact('tasks'));
+    }
+
+    public function show($id){
+     $task = App\Task::find($id);
+     return view('show',compact('task'));
+    }
 }
--- lara3/routes/web.php
+++ lara3/routes/web.php
@@ -12,50 +12,55 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    // $tasks = DB::table('tasks')->get();
-    // $tasks = App\Task::all();
-    $tasks=App\Task::incomplete();
-
-    return view('welcome',compact('tasks'));
-});
+Route::get('/','TaskController@index' );
+Route::get('/{task}','TaskController@show' );
 
 
-Route::get('/{task}', function ($id) {
-    // $task = DB::table('tasks')->find($id);
-    $task = App\Task::find($id);
 
-    // dd($task);
-    return view('show',compact('task'));
-});
 
-Route::get('hello', function () {
-    //способы передачи во вью
-    // в массиве вызов в коде {{$name}} или <?=$name 7>
-    // return view('hello',[
-    //     'name'=>'Alex'
-    // ]);
-    // второй вариант
-    // return view('hello')->with('name','alexey');
-    //третий вариант
-    // $name="Vladislav";
-    // return view('hello',['name'=>$name]);
-    //четвертый вариант compact обрпатная операция extract
-    // $name="Vsevolod";
-    // return view('hello',compact('name'));
-    // передаем массивы
-    $name=['add task','find task','remove task'];
-    return view('hello',compact('name'));
-    /* вызов в блэйде
-        <ul>
-            <?php foreach ($name as $n):?>
-            <li><?=$n?></li>
-            <?php endforeach; ?>
-        </ul>
-    или в нотации хелпера
-    @foreach ($name as $n)
-      <li>{{$n}}</li>
-    @endforeach
-    */
-});
+// Route::get('/', function () {
+//     // $tasks = DB::table('tasks')->get();
+//     // $tasks = App\Task::all();
+//     $tasks=App\Task::incomplete();
+
+//     return view('welcome',compact('tasks'));
+// });
+
+
+// Route::get('/{task}', function ($id) {
+//     // $task = DB::table('tasks')->find($id);
+//     $task = App\Task::find($id);
+
+//     // dd($task);
+//     return view('show',compact('task'));
+// });
+
+// Route::get('hello', function () {
+//     //способы передачи во вью
+//     // в массиве вызов в коде {{$name}} или <?=$name 7>
+//     // return view('hello',[
+//     //     'name'=>'Alex'
+//     // ]);
+//     // второй вариант
+//     // return view('hello')->with('name','alexey');
+//     //третий вариант
+//     // $name="Vladislav";
+//     // return view('hello',['name'=>$name]);
+//     //четвертый вариант compact обрпатная операция extract
+//     // $name="Vsevolod";
+//     // return view('hello',compact('name'));
+//     // передаем массивы
+//     $name=['add task','find task','remove task'];
+//     return view('hello',compact('name'));
+//     /* вызов в блэйде
+//         <ul>
+//             <?php foreach ($name as $n):7>
+//             <li><?=$n 7></li>
+//             <?php endforeach; 7>
+//         </ul>
+//     или в нотации хелпера
+//     @foreach ($name as $n)
+//       <li>{{$n}}</li>
+//     @endforeach
+//     */
+// });
