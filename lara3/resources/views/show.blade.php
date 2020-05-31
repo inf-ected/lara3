0a757bcbee32748bd2bc5d49c831379e1a006dfb
Infy <infarion@gmail.com>
2020-05-31 16:20:57 +0300

    db connect and simple queries

--- lara3/resources/views/show.blade.php
+++ lara3/resources/views/show.blade.php
@@ -7,14 +7,8 @@
     <title>Document</title>
 </head>
 <body>
-    <ul>
-        @foreach ($tasks as $task)
-        <li>
-        <a href="\{{$task->id}}">
-                {{$task->body}}
-            </a>
-        </li>
-        @endforeach
-    </ul>
+    <p>
+        {{$task->body}}
+       </p>
 </body>
 </html>
--- lara3/routes/web.php
+++ lara3/routes/web.php
@@ -14,10 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $tasks = DB::table('tasks')->get();
+    return view('welcome',compact('tasks'));
 });
 
 
+Route::get('/{task}', function ($id) {
+    $task = DB::table('tasks')->find($id);
+    // dd($task);
+    return view('show',compact('task'));
+});
 
 Route::get('hello', function () {
     //способы передачи во вью
